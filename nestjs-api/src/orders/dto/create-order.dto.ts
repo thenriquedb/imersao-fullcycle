75304d9221e0fa39a2f494656f873f1c9ba07d1b
Thiago Domingues <thenrique2012@gmail.com>
2024-01-31 19:03:01 -0300

    feat(nestjs-api): configure validation

--- nestjs-api/src/orders/dto/create-order.dto.ts
+++ nestjs-api/src/orders/dto/create-order.dto.ts
@@ -1,9 +1,26 @@
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsInt, IsNotEmpty, IsPositive, IsString, IsUUID, MaxLength, ValidateNested } from "class-validator";
+
 export class CreateOrderDto {
-  items: OrderItemDto[];
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
   card_hash: string;
+
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
 
 export class OrderItemDto {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
   quantity: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
   product_id: string;
 }
--- nestjs-api/src/products/dto/create-product.dto.ts
+++ nestjs-api/src/products/dto/create-product.dto.ts
@@ -1,7 +1,21 @@
+import { IsNotEmpty, IsPositive, IsString, IsUUID, } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   image_url: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsPositive()
+  @IsNotEmpty()
   price: number;
 }
