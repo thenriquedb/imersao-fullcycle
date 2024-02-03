34c8d16c32419b5fece0317143b3f1e003f00b1a
Thiago Domingues <thenrique2012@gmail.com>
2024-02-03 16:53:04 -0300

    chore(nestjs-api): rename orders queue

--- nestjs-api/src/orders/orders.consumer.ts
+++ nestjs-api/src/orders/orders.consumer.ts
@@ -18,7 +18,7 @@ export class OrderConsumer {
   @RabbitSubscribe({
     exchange: 'amq.direct',
     routingKey: 'PaymentDone',
-    queue: 'micro-orders'
+    queue: 'payments'
   })
   async consume(msg: Message) {
     try {
